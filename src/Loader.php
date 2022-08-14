0db51d713792ce224f89d14866f2bdef3bc90c47
Puleeno Nguyen <puleeno@gmail.com>
2022-08-14 17:07:28 +0700

    create instance for loader

--- src/Loader.php
+++ src/Loader.php
@@ -3,4 +3,16 @@ namespace Jankx\FullPage;
 
 class Loader
 {
+    protected static $instance;
+
+    protected function __construct()
+    {
+    }
+
+    public function getInstance() {
+        if (is_null(static::$instance)) {
+            static::$instance = new static();
+        }
+        return static::$instance;
+    }
 }
